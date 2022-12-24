278f196c42ac1c1b1e1afcacd243a961f21e9c98
Kudzai Gopfa <kudzaigopfa3@gmail.com>
2022-12-24 15:03:24 +0000

    UPDATE Refactor Lexical Analyser

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,69 +1,63 @@
-use std::io;
-use std::sync::{Arc, Mutex};
+use std::io::{self, BufRead};
 use std::str;
 
 mod constants;
 use constants::{EOI, SEMI, PLUS, TIMES, LP, RP, NUM_OR_ID};
 
-include!("../shared/vars/lexers.rs");
-
-lazy_static! {
-    // Initialize the shared variables
-    static ref LEXEME: Arc<Mutex<String>> = Arc::new(Mutex::new(String::new()));
-    static ref LEXEME_LENGTH: Arc<Mutex<usize>> = Arc::new(Mutex::new(0));
-    static ref LINE_NUMBER: Arc<Mutex<usize>> = Arc::new(Mutex::new(0));
-}
-
-fn lex() -> i32 {
-    let mut input_buffer = String::new();
-    let mut current;
-
-    current = LEXEME + LEXEME_LENGTH;
+fn lex(lexeme: String, lexeme_length: i32, line_number: i32) -> i32 {
+    let mut input_buffer: String = String::new();
+    let mut line_number: i32 = 0;
 
     loop {
-        while !*current {
-            current = &input_buffer;
-            if !io::stdin().read_line(&mut input_buffer).unwrap() {
-                *current = '\0';
-                return EOI;
-            }
-            LINE_NUMBER += 1;
+        let mut input_buffer: String = String::new();
+        let mut lexeme_start: i32 = 0;
 
-            while is_space(*current) {
-                current += 1;
-            }
-        }
+        match io::stdin().lock().read_line(&mut input_buffer) {
+            Err(error) => return EOI,
+            Ok(lexeme) => {
+                let line_length: i32 = lexeme.chars().count();
+                line_number += 1;
 
-        while *current {
-            LEXEME = current;
-            LEXEME_LENGTH = 1;
-
-            match *current {
-                'EOF' => return EOI,
-                '=' => return ASSIGN,
-                ';' => return SEMI,
-                '+' => return PLUS,
-                '-' => return MINUS,
-                '*' => return TIMES,
-                '/' => return DIVIDE,
-                '(' => return LP,
-                ')' => return RP,
-                '\n' => break,
-                '\t' => break,
-                ' ' => break,
-                _ => {
-                    if !isalnum(*current) {
-                        eprintln!("Ignoring illegal input: <{}>", *current);
-                    } else {
-                        while isalnum(*current) {
-                            current += 1;
-                        }
-
-                        LEXEME_LENGTH = current - LEXEME;
-                        return NUM_OR_ID;
+                for (i, c) in lexeme.chars().enumerate() {
+                    if !c.is_whitespace() {
+                        lexeme_start = i;
+                        break;
                     }
+                }
 
-                    break;
+                for i in lexeme_start..line_length-1 {
+                    let b: u8 = lexeme.as_bytes()[i];
+                    let current: char = b as char;
+
+                    match current {
+                        'EOF' => return EOI,
+                        '=' => return ASSIGN,
+                        ';' => return SEMI,
+                        '+' => return PLUS,
+                        '-' => return MINUS,
+                        '*' => return TIMES,
+                        '/' => return DIVIDE,
+                        '(' => return LP,
+                        ')' => return RP,
+                        '\n' => break,
+                        '\t' => break,
+                        ' ' => break,
+                        _ => {
+                            if !isalnum(current) {
+                                eprintln!("Ignoring illegal input: <{}>", current);
+                            } else {
+                                // @ToDo: Re-evaluate this logic
+                                while isalnum(current) {
+                                    current += 1;
+                                }
+        
+                                lexeme_length = current - LEXEME;
+                                return NUM_OR_ID;
+                            }
+        
+                            break;
+                        }
+                    }
                 }
             }
         }
